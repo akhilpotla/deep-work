e424158bdfce9b82205896dc29a2b212b0bd0dee
Akhil Potla <akhil4400@gmail.com>
2023-02-04 21:48:55 -0700

    Close right most tab

--- popup.js
+++ popup.js
@@ -1,7 +1,15 @@
 const tabs = await chrome.tabs.query({});
 
-console.log("TABS: " + JSON.stringify(tabs[0]));
-console.log("TABS: " + tabs.length);
-
 const count = document.getElementById("count");
 count.innerHTML = tabs.length;
+
+const closer = document.getElementById("closer");
+closer.onclick = async (e) => {
+  const tabId = getLastElement(tabs).id;
+  chrome.tabs.remove(tabId);
+}
+
+const getLastElement = (array) => {
+  const length = array.length;
+  return array[length - 1];
+}
