6865319a01a0566eec531668c0f64fdcd3d524b0
Akhil Potla <akhil4400@gmail.com>
2023-02-09 02:35:26 -0700

    Input field to change the maximum number of tabs

--- popup.js
+++ popup.js
@@ -3,9 +3,9 @@ const tabs = await chrome.tabs.query({});
 const count = document.getElementById("count");
 count.innerHTML = tabs.length;
 
-const closer = document.getElementById("closer");
-closer.onclick = async (e) => {
-  const lastTab = tabs[tabs.length - 1]
-  const tabId = lastTab.id;
-  chrome.tabs.remove(tabId);
+const saver = document.getElementById("saver");
+console.log("saver: " + saver);
+const max_tabs = document.getElementById("max-tabs");
+max_tabs.onchange = (e) => {
+  console.log(e.target.value);
 }
