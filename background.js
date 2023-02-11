17b4635865a4a6fe08582307b55ccd5606308f6e
Akhil Potla <akhil4400@gmail.com>
2023-02-11 15:45:49 -0700

    Setting default maximum number of tabs on install

--- background.js
+++ background.js
@@ -2,6 +2,14 @@
 // https://developer.chrome.com/docs/extensions/mv3/service_workers/
 // https://developer.chrome.com/docs/extensions/reference/runtime/#event-onInstalled
 
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.local.get(["MAX"]).then((result) => {
+    if (result === undefined || result.MAX === undefined) {
+      chrome.storage.local.set({ MAX: 3 });
+    }
+  });
+});
+
 chrome.tabs.onCreated.addListener(async () => {
   let tabs = await chrome.tabs.query({});
   let nTabs = tabs.length;
