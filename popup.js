070e8fc9eaff6b33e18a7eb509066540b9f4cda2
Akhil Potla <akhil4400@gmail.com>
2023-02-11 15:49:10 -0700

    Getter element no longer exists

--- popup.js
+++ popup.js
@@ -2,7 +2,6 @@ const tabs = await chrome.tabs.query({});
 
 const saver = document.getElementById("saver");
 const max_tabs = document.getElementById("max-tabs");
-const getter = document.getElementById("getter");
 
 saver.onsubmit = (e) => {
   e.preventDefault();
