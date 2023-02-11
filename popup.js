91ec8acbe86f34c19bde39be51be796ed965c9fb
Akhil Potla <akhil4400@gmail.com>
2023-02-11 15:19:25 -0700

    Removed tabs count, get max tab button, and unnecessary console.log

--- popup.js
+++ popup.js
@@ -1,21 +1,10 @@
 const tabs = await chrome.tabs.query({});
 
-const count = document.getElementById("count");
-count.innerHTML = tabs.length;
-
 const saver = document.getElementById("saver");
 const max_tabs = document.getElementById("max-tabs");
 const getter = document.getElementById("getter");
 
 saver.onsubmit = (e) => {
   e.preventDefault();
-  chrome.storage.local.set({ MAX: max_tabs.value }).then(() => {
-    console.log("Maximum number of tabs is set to: " + max_tabs.value);
-  });
-}
-
-getter.onclick = (e) => {
-  chrome.storage.local.get(["MAX"]).then((result) => {
-    console.log("Saved maximum number of tabs: " + result.MAX);
-  });
+  chrome.storage.local.set({ MAX: max_tabs.value });
 }
