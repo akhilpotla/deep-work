9581e3a3d083e8e75789c602fb62ec42ba0dfb4f
Akhil Potla <akhil4400@gmail.com>
2023-02-11 15:10:21 -0700

    Using maximum tabs from storage

--- background.js
+++ background.js
@@ -2,14 +2,14 @@
 // https://developer.chrome.com/docs/extensions/mv3/service_workers/
 // https://developer.chrome.com/docs/extensions/reference/runtime/#event-onInstalled
 
-const MAXIMUM_NUMBER_OF_TABS = 3;
-
 chrome.tabs.onCreated.addListener(async () => {
   let tabs = await chrome.tabs.query({});
   let nTabs = tabs.length;
-  if (nTabs > MAXIMUM_NUMBER_OF_TABS) {
-    const lastTab = tabs[tabs.length - 1];
-    const tabId = lastTab.id;
-    chrome.tabs.remove(tabId);
-  }
+  chrome.storage.local.get(["MAX"]).then((result) => {
+    if (nTabs > result.MAX) {
+      const lastTab = tabs[tabs.length - 1];
+      const tabId = lastTab.id;
+      chrome.tabs.remove(tabId);
+    }
+  });
 });
