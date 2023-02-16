314cb4b56c2723464dc1b3d0222756dda0acd6b1
Akhil Potla <akhil4400@gmail.com>
2023-02-15 21:17:52 -0700

    JS variables should be camelcase not underscored

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 const tabs = await chrome.tabs.query({});
 
 const saver = document.getElementById("saver");
-const max_tabs = document.getElementById("max-tabs");
+const maxTabs = document.getElementById("max-tabs");
 
 saver.onsubmit = (e) => {
   e.preventDefault();
-  chrome.storage.local.set({ MAX: max_tabs.value });
+  chrome.storage.local.set({ MAX: maxTabs.value });
 }
