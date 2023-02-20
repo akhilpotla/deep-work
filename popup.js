23309733b8de6f1375f5e87d33e3b3aa27588efb
Akhil Potla <akhil4400@gmail.com>
2023-02-19 20:45:01 -0700

    On/Off switch reflects the status of the extension

--- popup.js
+++ popup.js
@@ -5,11 +5,20 @@ const saver = document.getElementById("saver");
 const maxTabs = document.getElementById("max-tabs");
 const onOffSwitch = document.getElementById("on-off-switch");
 
+const status = await chrome.storage.local.get(["STATUS"])
+if (status === "ON") {
+  onOffSwitch.innerHTML = "Off";
+} else {
+  onOffSwitch.innerHTML = "On";
+}
+
+
 const result = await chrome.storage.local.get(["MAX"]);
 counter.innerHTML = result.MAX;
 
 saver.onsubmit = (e) => {
   e.preventDefault();
+  counter.innerHTML = maxTabs.value;
   chrome.storage.local.set({ MAX: maxTabs.value });
 }
 
@@ -19,8 +28,10 @@ onOffSwitch.onclick = (e) => {
     if (result !== undefined  || result.STATUS !== undefined) {
       if (result.STATUS === "ON") {
         chrome.storage.local.set({ STATUS: "OFF" });
+        onOffSwitch.innerHTML = "Off";
       } else {
         chrome.storage.local.set({ STATUS: "ON" });
+        onOffSwitch.innerHTML = "On";
       }
     }
   });
